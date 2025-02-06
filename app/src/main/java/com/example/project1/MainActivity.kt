b265e00642b6ee72a5f97ea993000eda396c0922
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-02-06 12:19:36 -0500

    added images

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -16,5 +16,13 @@ class MainActivity : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+
+
+
+
+
+
+
+
     }
 }
