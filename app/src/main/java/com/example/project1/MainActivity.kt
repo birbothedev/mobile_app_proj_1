3ab95913fd1011340e5e1e70fee7b314723f5f1e
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-02-06 12:36:23 -0500

    changed names of images

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.project1
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,12 +18,24 @@ class MainActivity : AppCompatActivity() {
 //            insets
 //        }
 
+        val paper : Button = findViewById(R.id.paper)
+        val rock : Button = findViewById(R.id.rock)
+        val scissors : Button = findViewById(R.id.scissors)
+        var playerChoice: String = ""
+        var computerChoice: String = ""
+        var winningChoice: String = ""
 
+        paper.setOnClickListener {
+            println("Paper Clicked")
+        }
 
+        rock.setOnClickListener {
+            println("Rock Clicked")
+        }
 
-
-
-
+        scissors.setOnClickListener {
+            println("Scissors Clicked")
+        }
 
     }
 }
